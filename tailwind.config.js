a52087138c69fb3778b1491b8a3caba78a72510d
McleanDesignCo <95174227+McleanDesignCo@users.noreply.github.com>
2023-03-08 11:29:33 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      aspectRatio: {
+        '4/3': '4 / 3',
+        '3/2': '3 / 2',
+    },
+  },
+  plugins: [],
+},
+}
